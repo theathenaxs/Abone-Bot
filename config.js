8950c50d305a9b41c957a95225ea05cb2c839eff
theathenaxs <165322512+theathenaxs@users.noreply.github.com>
2024-04-03 18:43:51 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    token: "", //bot tokeniniz
+    clientId: "", //Bot idsi
+    key: "", //ocr.space key link : https://ocr.space
+
+    //Alttakileri elleme !
+    langs: [
+        'Subscribed',
+        'Aboneolundu',
+        'Abunəoldu',
+        'Abone olundu',
+        'Abunə oldu',
+        'Gezeichnet',
+        'Abonné',
+        'Εγγεγραμμένος'
+    ],
+}
+
+// Maded By @sanalmuz
+// Maded By @sanalmuz
